6dbc858c981cf2b9ad4b23d7c74951e1ce91dbb5
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2023-10-06 07:35:47 -0700

    main page hogya hai dynamic karna baqi hai

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,10 +4,86 @@ export default function Rightbar(){
     return (
         <div className='rightbar'>
             <div className="rightbarwrapper">
-                <div className="birthdaycounter">
+                <div className="birdthdaycontainer">
                     <img className='birthdayimg' src="https://images.unsplash.com/photo-1549465220-1a8b9238cd48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmlydGhkYXklMjBnaWZ0fGVufDB8fDB8fHww&w=1000&q=80" alt="" />
-                    <span className="birthdaytext"><b>Zain</b> and <b>3 other frinds</b> have birthday today</span>
+                    <span className="birthdaytext">
+                        <b>Zain</b> and <b>3 other frinds</b> have birthday today
+                        </span>
                 </div>
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/013/057/701/small/ad-initial-letter-gold-calligraphic-feminine-floral-hand-drawn-heraldic-monogram-antique-vintage-style-luxury-logo-design-premium-vector.jpg" className='rightbarad' alt="" />
+                <h4 className="rightbartitle">Online Friends</h4>
+                <ul className="rightbarfriendlist">
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                    <li className="rightbarfriend">
+                        <div className="rightbarprofileimgcontainer">
+                            <img className='rightbarprofileimg' src="https://t4.ftcdn.net/jpg/04/76/04/99/360_F_476049927_ogzHW1dr6WBAg4Vlaz76NThq0k0bF17y.jpg" alt="" />
+                            <div className="rightbaronline"></div>
+                        </div>
+                        <span className="rightbarusername">Ismail</span>
+                    </li>
+                </ul>
             </div>
         </div>
     )
