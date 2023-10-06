ba0eb52813f3cec6d9869177f9ab8166a0f2d0b8
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-05 23:56:28 -0700

    1: 16: 12

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,12 @@ import './rightbar.css'
 export default function Rightbar(){
     return (
         <div className='rightbar'>
-            rightbar
+            <div className="rightbarwrapper">
+                <div className="birthdaycounter">
+                    <img className='birthdayimg' src="https://images.unsplash.com/photo-1549465220-1a8b9238cd48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmlydGhkYXklMjBnaWZ0fGVufDB8fDB8fHww&w=1000&q=80" alt="" />
+                    <span className="birthdaytext"><b>Zain</b> and <b>3 other frinds</b> have birthday today</span>
+                </div>
+            </div>
         </div>
     )
 }
